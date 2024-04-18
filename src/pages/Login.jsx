12e58bdd2119cad184c5c172267d54214fc585ad
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2024-04-18 11:51:02 +0200

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { logo } from '../assets'
-import Home from './Home'
+import React from "react";
+import { logo } from "../assets";
+import Home from "./Home";
 
 const Login = () => {
   return (
-    <div className='login-container'>
+    <div className="login-container">
       <img src={logo} alt="logo" />
-      <div className='login-layout'>
+      <div className="login-layout">
         <h1>Log in</h1>
         <form action="">
-            <label htmlFor="">Email</label>
-            <input type="email" placeholder='johndoe@gmail.com' />
-            <label htmlFor="">Password</label>
-            <input type="password" placeholder='..........' />
+          <label htmlFor="">Email</label>
+          <input type="email" placeholder="johndoe@gmail.com" />
+          <label htmlFor="">Password</label>
+          <input type="password" placeholder=".........." />
         </form>
         <p>forgot password?</p>
-        <button><a href="/home">GET IN</a></button>
+        <button>
+          <a href="/home">GET IN</a>
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
